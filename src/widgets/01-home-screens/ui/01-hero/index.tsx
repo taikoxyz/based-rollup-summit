c53664f36c2cb11c468d941203519df0783f73aa
swarnabhasinha <swarnabhasinha@gmail.com>
2025-03-03 16:11:05 +0530

    fix: responsive issues

--- src/widgets/01-home-screens/ui/01-hero/index.tsx
+++ src/widgets/01-home-screens/ui/01-hero/index.tsx
@@ -4,76 +4,64 @@ import ArrowLinkIcon from "shared/icons/ArrowLink.icon";
 import { NextLink } from "shared/ui/NextLink";
 import css from "./hero.module.scss";
 
-const CardsTemplate1: React.FC = () => {
+const CardsTemplate1 = () => {
     return (
-        <div className={css.cards}>
-            <div>
-                <div className={clsx(css.cards_wrapper)}>
-                    <NextLink
-                        href="https://lu.ma/37ykg387"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={css.cards_link}
-                    >
-                        <span
-                            className={css.cards_link_text}
-                            dangerouslySetInnerHTML={{ __html: "Register<br />now" }}
-                        />
-                        <ArrowLinkIcon className={css.cards_link_link} />
-                    </NextLink>
-                </div>
+        <NextLink
+            href="https://lu.ma/37ykg387"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css.cards_link}
+        >
+            <span
+                className={css.cards_link_text}
+                dangerouslySetInnerHTML={{ __html: "Register<br />now" }}
+            />
+            <div className={css.cards_link_link}>
+                <ArrowLinkIcon />
             </div>
-        </div>
+        </NextLink>
     );
 };
 
-const CardsTemplate2: React.FC = () => {
+const CardsTemplate2 = () => {
     return (
-        <div className={css.cards}>
-            <div>
-                <div className={clsx(css.cards_wrapper)}>
-                    <NextLink
-                        href="https://www.notion.so/taikoxyz/2025-Based-Rollup-Summit-Sponsor-Package-19e9673143d68087b018fd5c2679b937"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={css.cards_link_type}
-                    >
-                        <span
-                            className={css.cards_link_text}
-                            dangerouslySetInnerHTML={{ __html: "Apply as<br />Sponsor" }}
-                        />
-                        <ArrowLinkIcon className={css.cards_link_link_type} />
-                    </NextLink>
-                </div>
+        <NextLink
+            href="https://www.notion.so/taikoxyz/2025-Based-Rollup-Summit-Sponsor-Package-19e9673143d68087b018fd5c2679b937"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css.cards_link_type}
+        >
+            <span
+                className={css.cards_link_text}
+                dangerouslySetInnerHTML={{ __html: "Apply as<br />Sponsor" }}
+            />
+            <div className={css.cards_link_link_type}>
+                <ArrowLinkIcon />
             </div>
-        </div>
+        </NextLink>
     );
 };
 
-const CardsTemplate3: React.FC = () => {
+const CardsTemplate3 = () => {
     return (
-        <div className={css.cards}>
-            <div>
-                <div className={clsx(css.cards_wrapper)}>
-                    <NextLink
-                        href="https://docs.google.com/forms/d/1g-FnXyQ2qAsWpJ0cPY_2BGE-1mwkxTP0N-RXDV4YZfg/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={css.cards_link_type}
-                    >
-                        <span
-                            className={css.cards_link_text}
-                            dangerouslySetInnerHTML={{ __html: "Apply as<br />Speaker" }}
-                        />
-                        <ArrowLinkIcon className={css.cards_link_link_type} />
-                    </NextLink>
-                </div>
+        <NextLink
+            href="https://docs.google.com/forms/d/1g-FnXyQ2qAsWpJ0cPY_2BGE-1mwkxTP0N-RXDV4YZfg/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={css.cards_link_type}
+        >
+            <span
+                className={css.cards_link_text}
+                dangerouslySetInnerHTML={{ __html: "Apply as<br />Speaker" }}
+            />
+            <div className={css.cards_link_link_type}>
+                <ArrowLinkIcon />
             </div>
-        </div>
+        </NextLink>
     );
 };
 
-export const Hero: React.FC = () => {
+export const Hero = () => {
     return (
         <section className={css.hero}>
             <video className={css.hero_video} autoPlay loop muted playsInline>
@@ -88,7 +76,7 @@ export const Hero: React.FC = () => {
                 </div>
                 <div className={css.hero_right}>
                     <div className={css.hero_right_top}>
-                        <img src="/img/stars/pink.svg" />
+                        <img src="/img/stars/pink.svg" alt="" />
                         <span>The First-Ever</span>
                     </div>
                     <p>Summit Dedicated to Truly Decentralized Scaling Solutions.</p>
@@ -96,21 +84,19 @@ export const Hero: React.FC = () => {
             </div>
 
             <div className={css.container_bottom}>
-                <div className={css.hero_left}>
-                    <div className={css.bottom_left}>
-                        <div className={css.subheading}>
-                            <p>
-                                Join the brightest minds in the L2 ecosystem to explore, debate, and
-                                shape the future of Ethereum scalability
-                            </p>
-                        </div>
+                <div className={css.bottom_left}>
+                    <div className={css.subheading}>
+                        <p>
+                            Join the brightest minds in the L2 ecosystem to explore, debate, and
+                            shape the future of Ethereum scalability
+                        </p>
+                    </div>
 
-                        <div className={css.venue}>
-                            <img src="/img/icons/nav-arrow-right.svg" />
-                            <p>
-                                SAN FRANCISCO <span className={css.date}>MARCH 10, 2025</span>
-                            </p>
-                        </div>
+                    <div className={css.venue}>
+                        <img src="/img/icons/nav-arrow-right.svg" alt="" />
+                        <p>
+                            SAN FRANCISCO <span className={css.date}>MARCH 10, 2025</span>
+                        </p>
                     </div>
                 </div>
                 <div className={css.cardSection}>
