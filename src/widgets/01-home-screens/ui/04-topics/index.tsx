a814f3aafc2bf1c2c90aa4185924c946147c6255
swarnabhasinha <swarnabhasinha@gmail.com>
2025-03-03 19:58:05 +0530

    fix: topic alignment

--- src/widgets/01-home-screens/ui/04-topics/index.tsx
+++ src/widgets/01-home-screens/ui/04-topics/index.tsx
@@ -3,26 +3,32 @@ import { HOME_PAG } from "widgets/01-home-screens/lib";
 import css from "./topics.module.scss";
 
 export const Topics: React.FC = () => {
-    const topicsRef = React.useRef<HTMLDivElement>(null);
     const tags = [
-        { text: "Based Rollups", emoji: "🔥", color: "#FF6FC8" },
-        { text: "Booster Rollups", emoji: "🚀", color: "#FFC6E9" },
-        { text: "Interoperability", emoji: "🔗", color: "#FF6FC8" },
-        { text: "Synchronous Composability", emoji: "🔄", color: "#FFC6E9" },
-        { text: "Ecosystem", emoji: "🌐", color: "#FF6FC8" },
-        { text: "Preconfirmations", emoji: "⚡", color: "#FFC6E9" },
-        { text: "AI Integration", emoji: "💻", color: "#FF6FC8" },
-        { text: "zkEVMs", emoji: "🔒", color: "#FFC6E9" },
+        { text: "Based Rollups", emoji: "🔥" },
+        { text: "Booster Rollups", emoji: "🚀" },
+        { text: "Interoperability", emoji: "🔗" },
+        { text: "Synchronous Composability", emoji: "🔄" },
+        { text: "Ecosystem", emoji: "🌐" },
+        { text: "Preconfirmations", emoji: "⚡" },
+        { text: "AI Integration", emoji: "💻" },
+        { text: "zkEVMs", emoji: "🔒" },
     ];
+
     return (
-        <section className={css.topics} ref={topicsRef} id={HOME_PAG.TOPICS}>
+        <section className={css.topics} id={HOME_PAG.TOPICS}>
             <div className={css.topics_wrapper}>
-                <p className={css.topics_label}>Techincal Topics Covered?</p>
+                <h2 className={css.topics_label}>TECHNICAL TOPICS COVERED?</h2>
                 <div className={css.tagsContainer}>
                     {tags.map((tag, index) => (
-                        <span key={index} className={css.tag}>
-                            {tag.emoji} {tag.text}
-                        </span>
+                        <div
+                            key={index}
+                            className={css.tag}
+                            style={{
+                                backgroundColor: index % 2 === 0 ? "#ff6fc8" : "#ffc6e9",
+                            }}
+                        >
+                            <span>{tag.emoji}</span> {tag.text}
+                        </div>
                     ))}
                 </div>
             </div>
