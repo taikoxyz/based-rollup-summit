79036170677da77d1556f7a56a45c12387b4b0d2
bearni95 <bernatcanal@gmail.com>
2025-05-09 14:57:09 +0200

    lint

--- src/widgets/old/01-home-screens/ui/07-blog/index.tsx
+++ src/widgets/old/01-home-screens/ui/07-blog/index.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { useTranslation } from "next-i18next";
-import { useQuery } from "@tanstack/react-query";
-import { HomeApiKeys, HomeScreensEnum } from "widgets/old/01-home-screens/lib/types";
-import { IBlogsResponse } from "shared/lib/types";
+import { HomeScreensEnum } from "widgets/old/01-home-screens/lib/types";
 import css from "./blog.module.scss";
 
 const Blog: React.FC = () => {
-    const { data } = useQuery<IBlogsResponse>({ queryKey: [HomeApiKeys.BLOG] });
-    const { t } = useTranslation("home");
-
-    return (
-        <section className={css.blog} id={HomeScreensEnum.BLOG}>
-        </section>
-    );
+    return <section className={css.blog} id={HomeScreensEnum.BLOG}></section>;
 };
 
 export default Blog;
