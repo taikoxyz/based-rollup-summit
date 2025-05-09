6800c18c820d15472fd4b306bf312d1cc56b91df
bearni95 <bernatcanal@gmail.com>
2025-05-09 15:03:05 +0200

    splineless test

--- src/widgets/01-home-screens/ui/01-hero/index.tsx
+++ src/widgets/01-home-screens/ui/01-hero/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Spline from "@splinetool/react-spline/next";
 import css from "./hero.module.scss";
 
 // Define type for Button props
@@ -45,7 +44,6 @@ export const Hero: React.FC = () => {
                 {/* Main heading and details */}
                 <div className={css.content_wrapper}>
                     <div className={css.spline}>
-                        <Spline scene="/spline/scene.splinecode" />
                     </div>
                     <div className={css.main_content}>
                         <div className={css.heading_wrapper}>
