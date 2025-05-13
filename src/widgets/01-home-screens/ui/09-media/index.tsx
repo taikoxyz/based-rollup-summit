af54e36e408543280170cef728daa2f627d6f22a
bearni95 <bernatcanal@gmail.com>
2025-05-13 12:36:46 +0200

    updated sponsors

--- src/widgets/01-home-screens/ui/09-media/index.tsx
+++ src/widgets/01-home-screens/ui/09-media/index.tsx
@@ -14,23 +14,25 @@ interface EventPartner {
     height: number;
 }
 
-const eventPartners: EventPartner[] = [
+const diamondSponsors: EventPartner[] = [
+    { name: "Gattaca", logo: "", width: 226, height: 100 },
+    { name: "Ethgas", logo: "/img/media/ethgas.svg", width: 226, height: 100 },
+]
+
+const goldSponsors: EventPartner[] = [  
     { name: "OpenZeppelin", logo: "/img/media/openzeppelin.png", width: 226, height: 100 },
+
+]
+
+const silverSponsors: EventPartner[] = [
+    { name: "Fabric", logo: "/img/media/fabric.png", width: 226, height: 100 },
+    { name: "Commit - Boost", logo: "/img/media/commitboost.png", width: 226, height: 100 },
     { name: "Izumi", logo: "/img/media/izumi.svg", width: 226, height: 100 },
     { name: "Goldsky", logo: "/img/media/goldsky.png", width: 226, height: 100 },
     { name: "Prismacloud", logo: "/img/media/prismacloud.png", width: 226, height: 100 },
 
-    /*
-    { name: "Stanford Blockchain", logo: "/img/media/stanford.png", width: 226, height: 100 },
-    { name: "Blockchain Builders Fund", logo: "/img/media/BBF.png", width: 226, height: 100 },
-    { name: "Berkeley Blockchain", logo: "/img/media/berkeley.png", width: 226, height: 100 },
-    { name: "Columbia Blockchain", logo: "/img/media/columbia.png", width: 226, height: 100 },
-    { name: "Interstate", logo: "/img/media/interstate.png", width: 226, height: 100 },
-    { name: "Openledger", logo: "/img/media/openledger.png", width: 226, height: 100 },
-    { name: "Spire Labs", logo: "/img/media/spire.png", width: 226, height: 100 },
-    { name: "Rise Labs", logo: "/img/media/rise.png", width: 226, height: 100 },
-     */
-];
+]
+
 
 const mediaPartners: MediaPartner[] = [
     { name: "BeInCrypto", logo: "/img/media/beincrypto.png" },
@@ -70,12 +72,51 @@ export const Media: React.FC = () => {
 
     return (
         <section className={css.container} ref={mediaRef} id={HOME_PAG.MEDIA}>
+             {/* Event Partners Section */}
+             <div className={css.diamond_partners_label}>DIAMOND SPONSORS</div>
+
+<div className={css.partners}>
+    <div className={css.partners_grid}>
+        {diamondSponsors.map((partner, index) => (
+            <div key={index} className={css.partner_item}>
+                <img
+                    src={partner.logo}
+                    alt={`${partner.name}`}
+                    className={css.partnerLogo}
+                    width={partner.width}
+                    height={partner.height}
+                />
+            </div>
+        ))}
+    </div>
+</div>
+
+{/* Event Partners Section */}
+<div className={css.gold_partners_label}>GOLD SPONSORS</div>
+
+<div className={css.partners}>
+    <div className={css.partners_grid}>
+        {goldSponsors.map((partner, index) => (
+            <div key={index} className={css.partner_item}>
+                <img
+                    src={partner.logo}
+                    alt={`${partner.name}`}
+                    className={css.partnerLogo}
+                    width={partner.width}
+                    height={partner.height}
+                />
+            </div>
+        ))}
+    </div>
+</div>
+
+            
             {/* Event Partners Section */}
-            <div className={css.partners_label}>SPONSORS</div>
+            <div className={css.silver_partners_label}>SILVER SPONSORS</div>
 
             <div className={css.partners}>
                 <div className={css.partners_grid}>
-                    {eventPartners.map((partner, index) => (
+                    {silverSponsors.map((partner, index) => (
                         <div key={index} className={css.partner_item}>
                             <img
                                 src={partner.logo}
