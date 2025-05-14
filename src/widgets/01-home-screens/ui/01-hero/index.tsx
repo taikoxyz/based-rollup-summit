42f6e341d4dc42a84ed135c9be8ab0a9be86c8fb
bearni95 <bernatcanal@gmail.com>
2025-05-14 17:44:25 +0200

    cleanup

--- src/widgets/01-home-screens/ui/01-hero/index.tsx
+++ src/widgets/01-home-screens/ui/01-hero/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-//import { Application } from "@splinetool/runtime";
-// import dynamic from "next/dynamic";
+import React, { useEffect, useState } from "react";
 import css from "./hero.module.scss";
+
 // Define type for Button props
 interface ButtonProps {
     href: string;
@@ -17,29 +16,7 @@ const Button: React.FC<ButtonProps> = ({ href, text, className }) => {
         </a>
     );
 };
-/*
-const SplineScene = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-        try {
-            const canvas = canvasRef.current;
-            if (!canvas) return;
-
-            const app = new Application(canvas);
-            app.load("/spline/scene.splinecode");
-        } catch (error) {
-            console.warn("spline error", error);
-        }
-    }, []);
-
-    return <canvas id="spline" ref={canvasRef} />;
-};
-
-const StaticSplineScene = () => {
-    return <div className={css.static_spline}></div>;
-};
-*/
 export const Hero: React.FC = () => {
     const [isMobile, setIsMobile] = useState<boolean>(false);
 
@@ -61,18 +38,18 @@ export const Hero: React.FC = () => {
 
     const titleParts = ["BASED", "ROLLUP", "SUMMIT"];
 
-    const [isReady, setIsReady] = useState(false);
-    useEffect(() => {
-        setIsReady(true);
-    }, []);
-
     return (
         <section className={css.hero}>
             <div className={css.hero_content}>
                 {/* Main heading and details */}
                 <div className={css.content_wrapper}>
                     <div className={css.spline}>
-                    <iframe src='https://my.spline.design/brsblobbylandingpagecopy-CKuI3ckF5eo9uyyZhAowoGow/' frameBorder='0' width='100%' height='100%'></iframe>
+                        <iframe
+                            src="https://my.spline.design/brsblobbylandingpagecopy-CKuI3ckF5eo9uyyZhAowoGow/"
+                            frameBorder="0"
+                            width="100%"
+                            height="100%"
+                        ></iframe>
                     </div>
                     <div className={css.main_content}>
                         <div className={css.heading_wrapper}>
