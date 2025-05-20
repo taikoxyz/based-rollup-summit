3a1c17f66e7706be79554585239940bc42fc07f1
bearni95 <bernatcanal@gmail.com>
2025-05-20 15:18:08 +0200

    larger media partner icons

--- src/widgets/01-home-screens/ui/09-media/index.tsx
+++ src/widgets/01-home-screens/ui/09-media/index.tsx
@@ -23,7 +23,6 @@ const goldSponsors: EventPartner[] = [
     { name: "OpenZeppelin", logo: "/img/media/openzeppelin.png", width: 226, height: 100 },
     { name: "Avail", logo: "/img/media/avail.svg", width: 226, height: 100 },
     { name: "Nethermind", logo: "/img/media/nethermind.png", width: 226, height: 100 },
-
 ];
 
 const silverSponsors: EventPartner[] = [
