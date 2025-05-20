b7efb199e54366efe85b174ba6f86293ba054b6c
bearni95 <bernatcanal@gmail.com>
2025-05-20 11:13:45 +0200

    updated gold sponsors

--- src/widgets/01-home-screens/ui/09-media/index.tsx
+++ src/widgets/01-home-screens/ui/09-media/index.tsx
@@ -21,6 +21,9 @@ const diamondSponsors: EventPartner[] = [
 
 const goldSponsors: EventPartner[] = [
     { name: "OpenZeppelin", logo: "/img/media/openzeppelin.png", width: 226, height: 100 },
+    { name: "Avail", logo: "/img/media/avail.svg", width: 226, height: 100 },
+    { name: "Nethermind", logo: "/img/media/nethermind.png", width: 226, height: 100 },
+
 ];
 
 const silverSponsors: EventPartner[] = [
