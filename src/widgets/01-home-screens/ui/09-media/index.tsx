5703855fc9162e49736edb973ea55d91ec33e293
bearni95 <bernatcanal@gmail.com>
2025-05-26 15:10:55 +0200

    luban, not halborn

--- src/widgets/01-home-screens/ui/09-media/index.tsx
+++ src/widgets/01-home-screens/ui/09-media/index.tsx
@@ -23,7 +23,7 @@ const goldSponsors: EventPartner[] = [
     { name: "OpenZeppelin", logo: "/img/media/openzeppelin.png", width: 226, height: 100 },
     { name: "Avail", logo: "/img/media/avail.svg", width: 226, height: 100 },
     { name: "Nethermind", logo: "/img/media/nethermind.png", width: 226, height: 100 },
-    { name: 'Halborn', logo: '/img/media/halborn.jpeg', width: 226, height: 100 },
+    { name: 'Luban', logo: '/img/media/luban.jpeg', width: 226, height: 100 },
 ];
 
 const silverSponsors: EventPartner[] = [
